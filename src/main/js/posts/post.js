9c1d2dfb6b93b8bff8d0dd05f752ec025cdaddc3
Jose Macias <josemariamacias@gmail.com>
2019-05-03 12:06:16 +0100

    Added comments in index using the API

--- src/main/js/posts/post.js
+++ src/main/js/posts/post.js
@@ -8,6 +8,7 @@ class Post extends React.Component {
     this.state = {comments: [], likes: [], userid: document.getElementById("userid").value, toggle: null, likeid: 0};
     console.log(this.state.userid);
     this.getComments = this.getComments.bind(this);
+    this.sendComment = this.sendComment.bind(this);
     this.id = this.props.post._links.self.href.split("/")[this.props.post._links.self.href.split("/").length-1];
     this.Likes= this.Likes.bind(this);
     client({method: 'GET', path: '/api/likes/search/findByPostid?postid=' + this.id}).then(response => {
@@ -54,7 +55,16 @@ render () {
               				{this.getComments()}
               			</div>
             <a href={"post/"+this.id+"/comment"}>Comment!</a>
-		</div>
+            <form action="/">
+              <div class="form-group">
+                <input type="text" class="form-control" id="comment-text"  placeholder="Add comment"></input>
+                <button type="button" class="btn btn-primary" onClick={this.sendComment} >Submit</button>
+              </div>
+             </form>
+
+
+          </div>
+
 	)
     }
 
@@ -65,12 +75,33 @@ render () {
     			<Comment key={comment._links.self.href} comment={comment}/>
     		);
       }
-//
-//      buttonClass() {
-//        return this.state.toggle ? "btn btn-primary" : "btn btn-light"
-//      }
+
+      sendComment() {
+          if(this.state.userid !== "") {
+              const date = new Date();
+              console.log(document.getElementById("comment-text").value);
+              console.log("Begining fetch comment");
+              console.log(this.id);
+              fetch('/api/comments', {
+                        method: 'POST',
+                        headers: {
+                          'Accept': 'application/json',
+                          'Content-Type': 'application/json',
+                        },
+                        body: JSON.stringify({
+                          content: document.getElementById("comment-text").value ,
+                          time_stamp: date.getFullYear() + "/" + (date.getMonth() + 1) + "/" + date.getDate() + " " + date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds() ,
+                          postid: this.id,
+                        })
+                      });
+               document.location.reload(true);
+           }
+      }
+
+//let formatted_date = date.getFullYear() + "/" + (date.getMonth() + 1) + "/" + date.getDate() + " " + date.datetime.getHours() + ":" + date.datetime.getMinutes() + ":" + date.datetime.getSeconds()
 
       Likes() {
+
       if(this.state.userid !== "") {
       if(this.state.toggle) {
       console.log("Pre Fetch")
