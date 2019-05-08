121590a5f6306a25584aafa9b0cc3fa68e0c1e72
Josh Glasson <Josh.Glasson@tesco.com>
2019-05-08 09:12:06 +0100

    added flash message after data uptes

--- src/main/java/com/algorithmicefficiency/controller/HomeController.java
+++ src/main/java/com/algorithmicefficiency/controller/HomeController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import org.springframework.web.servlet.view.RedirectView;
 
 @Controller
@@ -24,12 +25,13 @@ public class HomeController {
 	}
 
 	@GetMapping(value = "/sort")
-	public RedirectView sort() {
+	public RedirectView sort(RedirectAttributes redirAttrs) {
 		Sort sort = new Sort();
 		sortRepository.deleteAll();
 		sort.start();
 		sortRepository.save(sort);
-//		System.out.println(sortRepository.findAll());
+		redirAttrs.addFlashAttribute("message", "Sort Data Refreshed: ");
+		redirAttrs.addFlashAttribute("newdata", ""+ sortRepository.findAll() +"");
 		return new RedirectView("/");
 	}
 
